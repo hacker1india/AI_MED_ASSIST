7afba4d6301ccab249507714d8443d778147aa74
Pasumarthi Bhanu prakash <115629228+hacker1india@users.noreply.github.com>
2025-10-09 11:10:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ st.markdown("""
 <style>
 body, h1, h2, h3, h4, h5, h6 { font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; }
 .card { padding:20px; border-radius:15px; box-shadow:0 5px 15px rgba(0,0,0,0.1); margin-bottom:15px; }
-.stButton>button { background-color:#138808;color:white;font-size:16px;font-weight:bold;border-radius:10px;padding:10px 20px;margin:5px 0px;border:none;transition: all 0.2s ease-in-out; }
+.stButton>button { background-color:#138808;color:grey;font-size:16px;font-weight:bold;border-radius:10px;padding:10px 20px;margin:5px 0px;border:none;transition: all 0.2s ease-in-out; }
 .stButton>button:hover { background-color:#0f6c06; transform:scale(1.05);}
 .response-text { font-size:18px;color:#000000; }
 .tab-header { color:#138808; font-weight:bold; }
